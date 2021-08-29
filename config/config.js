07bde21ce2151ce96737d3ac788572810485d91d
Sayantan Roy <sayantan.roy94@gmail.com>
2021-08-29 22:15:56 +0530

    updated config for timezones

--- config/config.js
+++ config/config.js
@@ -4,17 +4,29 @@ var { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 module.exports = {
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql"
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: true
+    },
+    timezone: '+05:30'
   },
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql"
+    username: DB_PASSWORD,
+    password: DB_PASSWORD,
+    database: DB_PASSWORD,
+    host: DB_PASSWORD,
+    dialect: "mysql",
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: true
+    },
+    timezone: '+05:30'
   }
 };
